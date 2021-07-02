054339cb6ddc21470ac46f7254ac7e56468f84c8
Vladykoo <vladykoo@mail.ru>
2021-07-02 15:10:11 +0300

    test webRTC

--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+// Vue.config.errorHandler = function(err, vm, info) {
+//   vm.$errors = err
+//   }
 
 new Vue({
   render: function (h) { return h(App) },
