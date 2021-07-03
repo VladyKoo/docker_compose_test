c7ddd063c3a1a71d1948aac285ce0ed3cec831dc
Vladykoo <vladykoo@mail.ru>
2021-07-03 04:17:32 +0300

    add head button

--- web/src/components/Camera.vue
+++ web/src/components/Camera.vue
@@ -2,18 +2,39 @@
   <div>
     <div class="camera">
       <div class="camera__container">
+          <div class="camera__header">
+            <div class="change-button" @click.prevent="changeCameras">
+              <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 20 20" height="48px" viewBox="0 0 20 20" width="48px" fill="#000000">
+                <g><rect fill="none" height="20" width="20" x="0"/></g><g><g><path d="M16,5h-2l-2-2H8L6,5H4C3.45,5,3,5.45,3,6v9c0,0.55,0.45,1,1,1h12c0.55,0,1-0.45,1-1V6C17,5.45,16.55,5,16,5z M16,15H4V6h2 h0.41l0.29-0.29L8.41,4h3.17l1.71,1.71L13.59,6H14h2V15z"/><path d="M10,6C9.28,6,8.62,6.2,8.04,6.54l0.73,0.73C9.15,7.1,9.56,7,10,7c1.65,0,3,1.35,3,3h-1.5l2,2l2-2H14C14,7.79,12.21,6,10,6 z"/><path d="M10,13c-1.65,0-3-1.35-3-3h1.5l-2-2l-2,2H6c0,2.21,1.79,4,4,4c0.72,0,1.38-0.2,1.96-0.54l-0.73-0.73 C10.85,12.9,10.44,13,10,13z"/></g></g>
+              </svg>
+            </div>
+            <div class="clear-button" @click.prevent="clearPhoto">
+              <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 0 24 24" width="48px" fill="#000000">
+                <path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
+              </svg>
+            </div>
+          </div>
         <div class="camera__main">
           <video ref="video" class="camera__video" autoplay playsinline>Video stream not available.</video>
-          <img v-show="image.url" :src="image.url" class="camera__img" alt="Capture image."> 
-
+          <img v-show="image.url" :src="image.url" class="camera__img" alt="Capture image.">           
         </div>
         <div class="camera__footer">
-          <button class="btn start-button" @click.prevent="start">Start stream</button>
+          <button class="btn start-button" :class="{'stop-button': striming}" @click.prevent="start">Start stream</button>
           <button class="btn take-button" @click.prevent="take"><div></div><div></div></button>
-          <button class="btn stop-button" @click.prevent="clearphoto">Clear photo</button>
-          <button class="btn stop-button" @click.prevent="clearphoto">Send photo</button>
+          <button class="btn send-button" @click.prevent="sendPhoto">Send photo</button>
         </div>
       </div>
+    </div>
+
+    <div v-if="currentDivice" class="device">
+      <div class="device__key">deviceId</div>
+      <div class="device__value">{{currentDivice.deviceId }}</div>
+      <div class="device__key">kind</div>
+      <div class="device__value">{{currentDivice.kind }}</div>
+      <div class="device__key">label</div>
+      <div class="device__value" >{{currentDivice.label }}</div>
+      <div class="device__key">groupId</div>
+      <div class="device__value">{{currentDivice.groupId }}</div>
     </div>
 
     <div>
@@ -21,6 +42,7 @@
         <li>error: {{ error }}</li>
       </ul>
     </div>
+
     <div v-for="(device, index) of devices" :key="index" class="device">
       <div class="device__key">deviceId</div>
       <div class="device__value">{{device.deviceId }}</div>
@@ -43,8 +65,8 @@ export default {
   name: 'Camera',
   data: () => ({
     url: 'http://localhost:80/api',
-    width: 640,
-    height: 480,
+    width: 0,
+    height: 0,
     video: null,
     canvas: null,
     error: null,
@@ -56,12 +78,10 @@ export default {
       quality: 1,
       type: 'image/png'
     },
-
-    supportedConstraints: []
+    striming: false,
   }),
 
   mounted(){  
-    this.video = this.$refs.video
     this.canvas = this.$refs.canvas
     this.getConnectedDevices('videoinput')
   },
@@ -122,6 +142,7 @@ export default {
           .then((stream) => {
             this.video.srcObject = stream
             this.getConnectedDevices('videoinput')
+            this.striming = true
           })
           .catch((err)=> {
             this.handleError(err)
@@ -134,6 +155,7 @@ export default {
           .then((stream) => {
             this.video.srcObject = stream
             this.getConnectedDevices('videoinput')
+            this.striming = true
           })
           .catch((err)=> {
             this.handleError(err)
@@ -146,6 +168,7 @@ export default {
           .then((stream) => {
             this.video.srcObject = stream
             this.getConnectedDevices('videoinput')
+            this.striming = true
           })
           .catch((err)=> {
             this.handleError(err)
@@ -155,6 +178,15 @@ export default {
     },
 
     start() {
+      if (this.striming) {
+        this.video.srcObject = null
+        this.striming = false
+        this.clearPhoto()
+        return
+      }
+
+      this.video = this.$refs.video
+
       const video = this.currentDivice && this.currentDivice.deviceId ? { 'deviceId': this.currentDivice.deviceId } : { facingMode: { exact: "environment" } }
       const constraints = {
         audio: false,
@@ -164,15 +196,26 @@ export default {
         .then((stream) => {
           this.video.srcObject = stream
           this.getConnectedDevices('videoinput')
+          this.striming = true
         })
         .catch((err)=> {
           this.handleError(err)
         });  
     },
 
-    clearphoto() {
+    clearPhoto() {
       this.image.url = ''
       this.image.blob = null
+    },
+
+    changeCameras(){
+      if (!this.currentDivice || this.devices.length < 2) return 
+      const newCurrentDivice = this.devices.filter((el) => el.deviceId !== this.currentDivice.deviceId)
+      this.currentDivice = newCurrentDivice[0]
+
+      this.striming = false
+      this.clearPhoto()
+      this.start()
     },
 
     take() {      
@@ -190,12 +233,13 @@ export default {
         this.image.url = URL.createObjectURL(blob)
       }, this.image.type, this.image.quality)  
     },
+
+    sendPhoto(){},
   }
 }
 </script>
 
 <style scoped>
-
 .camera__container {
   position: relative;
   max-width: 800px;
@@ -204,13 +248,30 @@ export default {
   padding: 30px 10px 50px 10px;
 }
 
+.camera__header {
+  background-color: black;
+  z-index: 5;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.change-button path {
+  fill: #fff
+}
+
+.clear-button path:nth-child(2) {
+  fill: #fff
+}
+
+.change-button:active, .clear-button:active {
+ transform: scale(.9);
+}
+
 .camera__main {
   position: relative;
   border: 1px solid #000;
   width: 100%;
-  /* border-top-right-radius: 10px;
-  border-top-left-radius: 10px; */
-  /* overflow: hidden; */
   background-color: grey;
 }
 
@@ -286,6 +347,10 @@ export default {
 
 .stop-button {
   background-color: rgb(255, 53, 53);
+}
+
+.send-button {
+  background-color: blue;
   border-radius: 10px;  
   padding: 10px;
   display: flex;
